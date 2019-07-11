25eb70b02301ec26f22c8cb4b5e179d240d1f47d
renchongwei123 <45593080+renchongwei123@users.noreply.github.com>
2019-07-11 15:53:35 +0800

    chang

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import router from './router'
 import fastClick from 'fastclick'
 import 'styles/reset.css'
@@ -10,6 +12,7 @@ import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper /* { default global options } */)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+   <div class="wrapper"> 
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" src="https://www.baidu.com/img/bd_logo1.png" alt="">
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+   </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data() {
+        return {
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList: [{
+                id: '0001',
+                imgUrl: ''
+            },{
+                id: '0001',
+                imgUrl: ''
+            },{
+                id: '0001',
+                imgUrl: ''
+            }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper>>> swiper-pagination-bullet-active
+    background red
+.wrapper
+    width 100%
+    height 0
+    overflow hidden
+    padding-bottom 31.25%
+    background-color #eee
+    .swiper-img
+        width 100%
+</style>
